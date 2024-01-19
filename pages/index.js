43461c7fb017f9e218f017b934343f10abf3f779
iSi2aka <n.ishizaka@outlook.com>
2024-01-19 23:13:22 +0900

    記述削除

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}zz
 export default function Home() {
   return (
     <Layout home>
